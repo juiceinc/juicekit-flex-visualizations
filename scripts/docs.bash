cf1efa9300ed79b7d524665b94370633668f6551
CGemignani <cgemignani@chris-gemignanis-macbook-pro-3.local>
2010-08-14 08:08:38 -0400

    Use custom templates for asdocs 

--- scripts/docs.bash
+++ scripts/docs.bash
@@ -2,5 +2,5 @@
 echo "Run from the main directory of juicekit-flex-visualizations to generate asdocs"
 echo ""
 echo "Output will be generated in target/asdoc-output/"
-asdoc -doc-sources+=src/main/flex -external-library-path+=../juicekit-flex-core/bin/juicekit-flex-core.swc -external-library-path+=../juicekit-flex-data/bin/juicekit-flex-data.swc -external-library-path+=../juicekit-flex-encode/bin/juicekit-flex-encode.swc -package-description-file scripts/package-description-visualizations.xml -main-title "JuiceKit Visualizations API Documentation" -output target/asdoc-output/
+asdoc -doc-sources+=src/main/flex -external-library-path+=../juicekit-flex-core/bin/juicekit-flex-core.swc -external-library-path+=../juicekit-flex-data/bin/juicekit-flex-data.swc -external-library-path+=../juicekit-flex-encode/bin/juicekit-flex-encode.swc -package-description-file scripts/package-description-visualizations.xml -main-title "JuiceKit Visualizations API Documentation" -output -templates-path ../juicekit-flex-core/scripts/templates target/asdoc-output/
 open target/asdoc-output/index.html
